dcd688583846e29fe12f068ddfa30c597c4f2551
Eitan Hemed <Eitan.Hemed@gmail.com>
2020-06-13 12:42:56 +0300

    Continued correlation and textual_report modules. Nothing is tested currently

--- robusta/correlations.py
+++ robusta/correlations.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 import robusta as rst
 
-__all__ = ['ChiSquare']
+__all__ = ['CorrChiSquare']
 
 
 class _BaseCorrelation:
 
-    def __init__(self, data=None, **kwargs):
+    def __init__(self, var1=None, var2=None, data=None, **kwargs):
         self.data = data
+        self.var1 = var1
+        self.var2 = var2
+        self.x, self.y = self.data[self.var1, self.var2].values.T
 
     def get_df(self):
         pass
@@ -23,11 +26,11 @@ class _BaseCorrelation:
         pass
 
 
-class ChiSquare(_BaseCorrelation):
-    def __init__(self, var1, var2, data=None, apply_correction=True):
-        self.var1 = var1
-        self.var2 = var2
-        self.data = self._crosstab_data(data)
+class CorrChiSquare(_BaseCorrelation):
+    def __init__(self, apply_correction=True,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.crosstabulated_data = self._crosstab_data(self.data)
         self.apply_correction = apply_correction
         self._r_results = self._run_analysis()
         self.results = self._finalize_results()
@@ -49,15 +52,69 @@ class ChiSquare(_BaseCorrelation):
         return f'{main_clause} {result_clause}.'
 
     def _crosstab_data(self, data):
-        data[[self.var1, self.var2]] = data[[self.var1, self.var2]].astype(
-            'category')
-        return pd.crosstab(
-            *data[[self.var1, self.var2]].astype('category').values.T)
+        return pd.crosstab(self.x, self.y)
 
     def _run_analysis(self):
-        return rst.pyr.rpackages.stats.chisq_test(self.data,
+        return rst.pyr.rpackages.stats.chisq_test(self.crosstabulated_data,
                                                   correct=self.apply_correction)
 
     def _finalize_results(self):
         return rst.utils.convert_df(
             rst.pyr.rpackages.generics.tidy(self._r_results))
+
+
+class CorrPearson(_BaseCorrelation):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._r_results = self._run_analysis()
+        self.results = self._finalize_results()
+
+    def _run_analysis(self):
+        return rst.pyr.rpackages.stats.cor_test(
+            x=self.x,
+            y=self.y,
+            method='pearson'
+        )
+
+
+class CorrSpearman(_BaseCorrelation):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._r_results = self._run_analysis()
+        self.results = self._finalize_results()
+
+    def _run_analysis(self):
+        return rst.pyr.rpackages.stats.cor_test(
+            x=self.x,
+            y=self.y,
+            method='spearman'
+        )
+
+
+class CorrKendall(_BaseCorrelation):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._r_results = self._run_analysis()
+        self.results = self._finalize_results()
+
+    def _run_analysis(self):
+        return rst.pyr.rpackages.stats.cor_test(
+            x=self.x,
+            y=self.y,
+            method='kendall'
+        )
+
+
+# TODO - see what other
+class BayesCorrelation(CorrPearson, **kwargs):
+
+    def __init__(self):
+        super().__init__(**kwargs)
+
+    def _run_analysis(self):
+        return rst.pyr.rpackages.bayes_factor.correlationBF(
+            x=self.x, y=self.y
+        )
--- robusta/textual_reports.py
+++ robusta/textual_reports.py
@@ -1,6 +1,34 @@
-import robusta  # So we can get the PyR singleton
+import numpy as np
+import robusta as rst
 
-class TextualReport:
+class _TextualReporter:
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, data, analysis_type, **kwargs) -> None:
+        self._verify_labels(data, analysis_type)
+
+    def process_frequentist_term(self, term_dict: dict, test_type: str = 'hypothesis',
+                                 term_type: str = ''):
+        """
+
+        @type test_type: object
+        @param test_type:
+        @type d: object
+        """
+        f" {self._process_significance_result(dat['p.value'])} significant effect between"
+        result_clause = (f"[\u03C7\u00B2({term_dict['parameter']:.0f})" +
+                         f" = {term_dict['statistic']:.2f}, p = {term_dict['p.value']:.3f}]")
+
+
+
+    def _process_significance_result(self, sig, alpha=.05):
+        return np.where(sig < alpha, 'a', 'no')
+
+    def _verify_labels(self, data):
+        pass
+
+class SingleTermReporter(_TextualReporter):
+    pass
+
+class MultiTermReporter(_TextualReporter):
+    pass
+
